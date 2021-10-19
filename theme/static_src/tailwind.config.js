f31caca2a3309a2672eed148e59b1696827b8108
Chan Rose <chanrose@pm.me>
2021-10-19 22:05:01 +0700

    add tailwind

--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -1,57 +1,41 @@
-/**
- * This is a minimal config.
- *
- * If you need the full config, get it from here:
- * https://unpkg.com/browse/tailwindcss@latest/stubs/defaultConfig.stub.js
- */
+const colors = require("tailwindcss/colors");
 
 module.exports = {
-  /**
-   * Stylesheet generation mode.
-   *
-   * Set mode to "jit" if you want to generate your styles on-demand as you author your templates;
-   * Set mode to "aot" if you want to generate the stylesheet in advance and purge later (aka legacy mode).
-   */
   mode: "jit",
 
   purge: [
-    /**
-     * HTML. Paths to Django template files that will contain Tailwind CSS classes.
-     */
-
-    /*  Templates within theme app (<tailwind_app_name>/templates), e.g. base.html. */
     "../templates/**/*.html",
-
-    /*
-     * Main templates directory of the project (BASE_DIR/templates).
-     * Adjust the following line to match your project structure.
-     */
     "../../templates/**/*.html",
-
-    /*
-     * Templates in other django apps (BASE_DIR/<any_app_name>/templates).
-     * Adjust the following line to match your project structure.
-     */
     "../../**/templates/**/*.html",
-
-    /**
-     * JS: If you use Tailwind CSS in JavaScript, uncomment the following lines and make sure
-     * patterns match your project structure.
-     */
-    /* JS 1: Ignore any JavaScript in node_modules folder. */
-    // '!../../**/node_modules',
-    /* JS 2: Process all JavaScript files in the project. */
-    // '../../**/*.js',
-
+    "../../../**/templates/**/*.html",
     /**
      * Python: If you use Tailwind CSS classes in Python, uncomment the following line
      * and make sure the pattern below matches your project structure.
      */
-    // '../../**/*.py'
+    // '../../**/*.py' 
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      white: colors.white,
+      gray: colors.gray,
+      bgray: colors.blueGray,
+      cgray: colors.coolGray,
+      wgray: colors.warmGray,
+      red: colors.red,
+      blue: colors.sky,
+      orange: colors.orange,
+      yellow: colors.yellow,
+      amber: colors.amber,
+      lime: colors.lime,
+      rose: colors.rose,
+      fuchsia: colors.fuchsia,
+      lime: colors.lime,
+      green: colors.green,
+      emerald: colors.emerald,
+      teal: colors.teal,
+    },
   },
   variants: {
     extend: {},
