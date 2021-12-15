71fe5f9f01403c8ed7e94b94985013f6fb91e9ab
Chan Rose <chanrose@pm.me>
2021-12-16 05:24:00 +0700

    updated testing by Abel

--- apps/test.py
+++ apps/test.py
@@ -3,7 +3,10 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, logout
 from django.test.client import RequestFactory, Client
 from apps.catalogue.abstract_models import AbstractProduct, AbstractProductCategory
+from account.models import Account 
 from django.urls import reverse
+from django.shortcuts import redirect
+from django.contrib.auth import authenticate, login
 
 # class UserTestCase(TestCase):
 
@@ -41,23 +44,32 @@ from django.urls import reverse
         # response = self.client.post(reverse('customer:change-password'), {'password': 'kate1234', 'email': 'kate@gmail.com'})
         # self.assertEquals(response.status_code, 200)
 
-class ProductTestCase(TestCase):
+# class ProductTestCase(TestCase):
 
-    def setUp(self):
-        category = AbstractProductCategory.objects.create(name='small-umbrella').save()  
-        product = AbstractProduct.objects.create(name='zag_umbrella', upc='UMB', categories=category).save()
-        print(product.name)
+#     def setUp(self):
+#         # category = AbstractProductCategory(name='small-umbrella').save()  
+#         product = AbstractProduct(name='zag_umbrella', upc='UMB', categories='umbrellas').save()
+#         print(category.name)
     
-    def test_update_product(self):
-        pass
+#     def test_update_product(self):
+#         pass
 
-# class ProfileTestCase(TestCase):
+class VoucherTestCase(TestCase):
+    
+    def setUp(self):
+        self.user = User.objects.create(username='kate', email='kate@gmail.com')
+        self.user.set_password('kate123')
+        self.user.save()
+        user = authenticate(username=str(self.user.username), password=str(self.user.password))
+        if user is not None:
+            print('authenticated')
+            login(self.request, user)
 
-#     def test_update_profile(self):
-#         # response = self.client.get(reverse('customer:profile-update'), follow=True)
-#         # self.assertEquals(response.status_code, 200)
-#         response = self.client.post(reverse('customer:profile-update'), {'first_name': 'jane', 'last_name': 'maylor'}, follow=True)
-#         self.assertEquals(response.status_code, 200)
+    def test_create_voucher(self):  
+        response = self.client.get(reverse('extra:redeemable_voucher'))
+        self.assertEquals(response.status_code, 200)
+
+        # self.assertEquals(response.status_code, 200)
         # self.client(reverse('customer:profile-view'), {})
         # self.assertTemplateUsed(response, 'oscar/customer/profile_form.html')
         
